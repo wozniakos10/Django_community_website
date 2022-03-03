670d941dd260c3512a47becb14f24d4e80329cd3
Dawid Wozniak <wozniakos10@gmail.com>
2022-03-03 18:38:56 +0100

    Register app

--- ms_web/ms_web/settings.py
+++ ms_web/ms_web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'msagh_website.apps.MsaghWebsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
