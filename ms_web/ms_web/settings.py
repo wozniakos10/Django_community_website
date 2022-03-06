05b30051539168ca81be6b1bd82d260943739bd7
Dawid Wozniak <wozniakos10@gmail.com>
2022-03-06 22:18:02 +0100

    Make new app

--- ms_web/ms_web/settings.py
+++ ms_web/ms_web/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'msagh_website.apps.MsaghWebsiteConfig',
+    'members.apps.MembersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
