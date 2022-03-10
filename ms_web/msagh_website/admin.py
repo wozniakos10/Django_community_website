f7252c7f6795fbc54b480661d5de6906d9588932
Dawid Wozniak <wozniakos10@gmail.com>
2022-03-10 23:56:11 +0100

    Finally work

--- ms_web/msagh_website/admin.py
+++ ms_web/msagh_website/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Spot
 # Register your models here.
 
-admin.site.register(Spot)
+class SpotAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'user','content')
+
+
+admin.site.register(Spot,SpotAdmin)
+
