9ee5f47817529f5a7291e5a2e81e632aed2b91f3
Dawid Woźniak <wozniakos10@gmail.com>
2022-04-07 20:11:58 +0200

    Adding reset password logic

--- ms_web/msagh_website/urls.py
+++ ms_web/msagh_website/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from. import views
+from django.contrib.auth import views as auth_views #import this
 
 app_name = "msagh_website"
 
@@ -10,4 +11,14 @@ urlpatterns = [
     path('memes/', views.memes, name="memes"),
     path('new_meme/',views.new_meme,name='new_meme'),
     path('contact/', views.contact, name="contact"),
+    path("password_reset/", views.password_reset_request, name="password_reset"),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="password/password_reset_confirm.html"), name='password_reset_confirm'),
+    # URL connected with passsword
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(  # reset functional
+        template_name='password/password_reset_done.html'), name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'),
+        name='password_reset_complete'),
+
 ]
