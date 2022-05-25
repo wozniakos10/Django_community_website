efca3cddb8b4bb5b6024be3280c2e9a98c951bbe
Dawid Woźniak <wozniakos10@gmail.com>
2022-05-26 00:29:55 +0200

    Adding password changing func/fix template

--- ms_web/msagh_website/urls.py
+++ ms_web/msagh_website/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path('new_meme/',views.new_meme,name='new_meme'),
     path('contact/', views.contact, name="contact"),
     path("password_reset/", views.password_reset_request, name="password_reset"),
+    path('password_change/',auth_views.PasswordChangeView.as_view(template_name = "password/password_change.html",
+                                                                  success_url="http://127.0.0.1:8000/"),name='password_change'),
 
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name="password/password_reset_confirm.html"), name='password_reset_confirm'),
